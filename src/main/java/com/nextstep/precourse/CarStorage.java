d91c8ce97ab40b290f00ff7e5261dfa3084ca236
Eum-YS <isoft1985@gmail.com>
2021-05-01 22:58:14 +0900

    Feat(CarStorage): 자동차 저장소
    기능 추가
    - Storage에 저장된 차 움직이는 함수 추가
    - Storage에 저장된 차 정보 반환 함수 추가
      Car 객체를 복사하여 Storage 저장된 데이터엔 변경이 없도록 함

--- src/main/java/com/nextstep/precourse/CarStorage.java
+++ src/main/java/com/nextstep/precourse/CarStorage.java
@@ -2,11 +2,12 @@ package com.nextstep.precourse;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class CarStorage implements Iterable<Car> {
-	private List<Car> racingCars;
+public class CarStorage {
+	public List<Car> racingCars;
 
 	public CarStorage(Collection<String> carNameList) {
 		racingCars = new ArrayList<>();
@@ -20,8 +21,32 @@ public class CarStorage implements Iterable<Car> {
 		racingCars.add(car);
 	}
 
-	@Override
-	public Iterator<Car> iterator() {
-		return racingCars.iterator();
+	public void moveCars() {
+		for (Car car : racingCars) {
+			car.tryToMove();
+		}
+	}
+
+	public Car[] getCars() {
+		Car[] cars = new Car[racingCars.size()];
+		for (int i = 0; i < racingCars.size(); i++) {
+			try {
+				cars[i] = racingCars.get(i).clone();
+			} catch (CloneNotSupportedException e) {
+				continue;
+			}
+		}
+		return cars;
+	}
+
+	public Collection<String> getFarthestMovedCarNames() {
+		int farthestDistance = 0;
+		Map<Integer, List<String>> distanceMap = new HashMap<>();
+		for (Car car : racingCars) {
+			distanceMap.computeIfAbsent(car.getDistance(),
+				key -> new ArrayList<String>()).add(car.getName());
+			farthestDistance = car.getDistance() > farthestDistance ? car.getDistance() : farthestDistance;
+		}
+		return distanceMap.get(farthestDistance);
 	}
 }
--- src/test/java/com/nextstep/precourse/CarStorageTest.java
+++ src/test/java/com/nextstep/precourse/CarStorageTest.java
@@ -4,24 +4,57 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CarStorageTest {
+	private static final String[] TEST_CAR_NAMES = new String[] {"pobi", "crong", "honux"};
+	private CarStorage testCarStorage;
+
+	@BeforeEach
+	public void createTestCarStorage() {
+		testCarStorage = new CarStorage(Arrays.asList(TEST_CAR_NAMES));
+	}
+
 	@Test
 	public void testCreatedCar() {
-		String[] carNames = new String[] {"pobi", "crong", "honux"};
-		CarStorage carStorage = new CarStorage(Arrays.asList(carNames));
-
 		List<String> createdCarNames = new ArrayList<>();
-		Iterator<Car> iter = carStorage.iterator();
-		while (iter.hasNext()) {
-			createdCarNames.add(iter.next().getName());
+		for (Car car : testCarStorage.getCars()) {
+			createdCarNames.add(car.getName());
 		}
-
 		assertThat(createdCarNames)
-			.containsExactly(carNames);
+			.containsExactly(TEST_CAR_NAMES);
+	}
+
+	@Test
+	public void testMoveCars() {
+		testCarStorage.moveCars();
+
+		assertThat(testCarStorage.getCars())
+			.extracting("tryCount").containsExactly(1, 1, 1);
+	}
+
+	@Test
+	public void testGetFarthestMovedCarNames() {
+		for (int i = 0; i < 10; i++) {
+			testCarStorage.moveCars();
+		}
+		Car[] cars = testCarStorage.getCars();
+		int farthestDistance = getFarthesDistance(cars);
+
+		assertThat(cars)
+			.filteredOn(car -> car.getDistance() == farthestDistance)
+			.extracting("name")
+			.containsAll(testCarStorage.getFarthestMovedCarNames());
+	}
+
+	private int getFarthesDistance(Car[] cars) {
+		int farthestDistance = 0;
+		for (Car car : cars) {
+			farthestDistance = car.getDistance() > farthestDistance ? car.getDistance() : farthestDistance;
+		}
+		return farthestDistance;
 	}
 }
